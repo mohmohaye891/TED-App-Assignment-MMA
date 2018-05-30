e3cc90aff28700596fd1c9a1e19647947578a15c
Unknown <mohmohaye8891@gmail.com>
2018-05-31 00:10:49 +0630

    Create NewestAdapter,NewestViewHolder and update Mainactivity MMA.

---
+++ app/src/main/java/com/padcmyanmar/ted/adapters/NewestAdapter.java
@@ -0,0 +1,30 @@
+package com.padcmyanmar.ted.adapters;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.padcmyanmar.ted.R;
+import com.padcmyanmar.ted.viewholders.NewestViewHolder;
+
+public class NewestAdapter extends RecyclerView.Adapter {
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.view_holder_talk, parent, false);
+        return new NewestViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 15;
+    }
+}
---
+++ app/src/main/java/com/padcmyanmar/ted/viewholders/NewestViewHolder.java
@@ -0,0 +1,10 @@
+package com.padcmyanmar.ted.viewholders;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class NewestViewHolder extends RecyclerView.ViewHolder {
+    public NewestViewHolder(View itemView) {
+        super(itemView);
+    }
+}
